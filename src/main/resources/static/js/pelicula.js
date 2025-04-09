fc3335003df211b49f9949ed1b48973541e149bf
javfalrea <javierfalconreal2003@gmail.com>
2025-04-09 17:39:35 +0200

    avance en el frontend. Hay que arreglar los filtros de búsqueda

--- src/main/resources/static/js/pelicula.js
+++ src/main/resources/static/js/pelicula.js
@@ -28,3 +28,40 @@ document.addEventListener("DOMContentLoaded", function() {
         .catch(error => console.error("Error al cargar los géneros:", error));
 });
 
+
+document.getElementById("buscarPelicula").addEventListener("click", function() {
+    // Recoger los valores de los filtros
+    const titulo = document.getElementById("tituloFiltro").value.trim() || "";
+    const participante = document.getElementById("participanteFiltro").value.trim() || "";
+    const idPais = document.getElementById("paisFiltro").value || null;
+    const idGenero = document.getElementById("generoFiltro").value || null;
+
+    // Crear la URL con los filtros
+    let url = "/buscar_peliculas?tituloBq=" + titulo + "&participanteBq=" + participante + "&idPaisBq=" + idPais + "&idGeneroBq=" + idGenero;
+
+    // Hacer la solicitud GET al backend
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Limpiar la tabla antes de mostrar nuevos resultados
+            const tabla = document.getElementById("tablaPeliculas").getElementsByTagName('tbody')[0];
+            tabla.innerHTML = "";
+
+            // Rellenar la tabla con los datos recibidos
+            data.forEach(pelicula => {
+                const row = tabla.insertRow();
+
+                row.innerHTML = `
+                    <td>${pelicula.titulo}</td>
+                    <td>${pelicula.anio_estreno}</td>
+                    <td>${pelicula.pais.nombre}</td>
+                    <td>${pelicula.duracion}</td>
+                `;
+            });
+        })
+        .catch(error => {
+            console.error("Error al cargar las películas:", error);
+        });
+});
+
+
